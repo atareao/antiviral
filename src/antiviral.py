6aa177592e0a878276d12dd2635498df37840081
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2020-08-15 07:55:39 +0200

    to clean version

--- src/antiviral.py
+++ src/antiviral.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#       antiviral.py
+# This file is part of antiviral
 #
-#       Copyright 2010 Lorenzo Carbonell <atareao@zorita>
+# Copyright (c) 2015 Lorenzo Carbonell Cerezo <a.k.a. atareao>
 #
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import gi
 try:
@@ -334,6 +337,7 @@ class Antiviral(Gtk.Window):
         self.save_preferences()
         self.hide()
         self.destroy()
+        exit(0)
 
     def toggled_cb(self, cell, path, user_data):
         model, column = user_data
@@ -578,6 +582,11 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 def main():
+    Antiviral()
+    Gtk.main()
+
+
+def test():
     print(pyclamav.version())
     print(pyclamav.get_version())
     print(pyclamav.get_numsig())
@@ -591,6 +600,4 @@ def main():
     return 00
 
 if __name__ == '__main__':
-    GObject.threads_init()
-    Antiviral()
-    Gtk.main()
+    main()
--- src/pyclamd.py
+++ src/pyclamd.py
@@ -53,6 +53,9 @@
 # 2015-03-14 v0.3.14 AN : - Bug correction for clamd.conf default path
 # 2015-06-04 v0.3.15 AN : - optimization in scan_stream
 # 2015-10-21 v0.3.16 JMS : - avoid EICAR detection in py3 pyc file
+# 2016-08-07 v0.3.17 AN: - typo change (Connexion to Connection)
+# 2017-08-27 v0.4.0 RC: - modified scan_stream() to add support for passing file-like objects
+#                   BM: - add allmatchscan with file and directory support
 #------------------------------------------------------------------------------
 # TODO:
 # - improve tests for Win32 platform (avoid to write EICAR file to disk, or
@@ -71,10 +74,12 @@ pyclamd.py
 
 Author : Alexandre Norman - norman()xael.org
 Contributors :
+ - BM : Brandon Murphy - bitbucket () zoomequipd.com
  - JB :  Joe Brandt  - brandt.joe () gmail.com
  - JMS: Jack Saunders - jack () oldstlabs.com
  - JS : Joni Salonen - joni.salonen () qindel.com
  - PL :  Philippe Lagadec - philippe.lagadec()laposte.net
+ - RC : Robert Coup
  - SK : Scott Kitterman - debian () kitterman.com
  - TK :  Thomas Kastner - tk()underground8.com
  - TKL : Thomas Kluyver - thomas () kluyver.me.uk
@@ -129,7 +134,7 @@ True
 >>> os.remove('/tmp/EICAR-éèô请收藏我们的网址')
 """
 
-__version__ = "0.3.17"
+__version__ = "0.4.0"
 
 
 # $Source$
@@ -396,7 +401,65 @@ class _ClamdGeneric(object):
 
 
 
+    def allmatchscan(self, file):
+        """
+        Scan a file or directory given by filename and after finding a virus within a file, continues scanning for additional viruses.
+        Scan with archive support enabled.
 
+        file (string) : filename or directoy (MUST BE ABSOLUTE PATH !)
+
+        return either :
+          - (dict): {filename1: [(FOUND', 'virusname1'), (FOUND', 'virusname2')], filename2:  [(FOUND', 'virusname1'), (FOUND', 'virusname3')]}
+          - None: if no virus found
+
+        May raise :
+          - ConnectionError: in case of communication problem
+          - socket.timeout: if timeout has expired
+        """
+        assert isstr(file), 'Wrong type for [file], should be a string [was {0}]'.format(type(file))
+
+        dr={}
+
+        if os.path.isdir(file):
+            for path, subdirs, files in os.walk(file):
+                for name in files:
+                     single_file_result = self.allmatchscan(os.path.join(path,name))
+                     if single_file_result:
+                         dr.update(single_file_result)
+
+        else:
+            try:
+                self._init_socket()
+                self._send_command('ALLMATCHSCAN {0}'.format(file))
+            except socket.error:
+                raise ConnectionError('Unable to scan {0}'.format(file))
+
+            result='...'
+
+            while result:
+                try:
+                    result = self._recv_response()
+                except socket.error:
+                    raise ConnectionError('Unable to scan {0}'.format(file))
+
+                if len(result) > 0:
+                    for resline in result.splitlines():
+                        filename, reason, status = self._parse_response(resline)
+
+                        if status == 'ERROR':
+                            if filename not in dr:
+                                dr[filename] = []
+                            dr[filename].append(('ERROR', '{0}'.format(reason)))
+
+                        elif status == 'FOUND':
+                            if filename not in dr:
+                                dr[filename] = []
+                            dr[filename].append(('FOUND', '{0}'.format(reason)))
+            self._close_socket()
+
+        if not dr:
+            return None
+        return dr
 
 
 
@@ -448,14 +511,14 @@ class _ClamdGeneric(object):
 
 
 
-    def scan_stream(self, buffer_to_test):
+    def scan_stream(self, stream, chunk_size=4096):
         """
         Scan a buffer
 
         on Python2.X :
-          - buffer_to_test (string): buffer to scan
+          - input (string): buffer to scan
         on Python3.X :
-          - buffer_to_test (bytes or bytearray): buffer to scan
+          - input (bytes or bytearray): buffer to scan
 
         return either:
           - (dict): {filename1: "virusname"}
@@ -467,10 +530,12 @@ class _ClamdGeneric(object):
         """
         if sys.version_info[0] <= 2:
             # Python2
-            assert isstr(buffer_to_test), 'Wrong type fom [buffer_to_test], should be str [was {0}]'.format(type(buffer_to_test))
+            assert hasattr(stream, "read") or isinstance(stream, str), 'Wrong type for [stream], should be str/file-like [was {0}]'.format(type(stream))
         else:
             # Python3
-            assert isinstance(buffer_to_test, bytes) or isinstance(buffer_to_test, bytearray), 'Wrong type fom [buffer_to_test], should be bytes or bytearray [was {0}]'.format(type(buffer_to_test))
+            assert hasattr(stream, "read") or isinstance(stream, (bytes, bytearray)), 'Wrong type for [stream], should be bytes/bytearray/file-like [was {0}]'.format(type(stream))
+
+        is_file_like = hasattr(stream, 'read')
 
         try:
             self._init_socket()
@@ -479,21 +544,33 @@ class _ClamdGeneric(object):
         except socket.error:
             raise ConnectionError('Unable to scan stream')
 
-        # MUST be < StreamMaxLength in /etc/clamav/clamd.conf
-        # or /etc/clamd.conf
-        max_chunk_size = 1024
+        if is_file_like:
+            while True:
+                chunk = stream.read(chunk_size)
+                if not chunk:
+                    break
+                size = struct.pack('!L', len(chunk))
+                try:
+                    self.clamd_socket.send(size)
+                    self.clamd_socket.send(chunk)
+                except socket.error:
+                    raise
 
-        for n in range(1 + int(len(buffer_to_test)/max_chunk_size)):
-            chunk = buffer_to_test[n*max_chunk_size:(n+1)*max_chunk_size]
-            size = struct.pack('!L', len(chunk))
-            try:
-                self.clamd_socket.send(size)
-                self.clamd_socket.send(chunk)
-            except socket.error:
-                break
-        else:
             # Terminating stream
             self.clamd_socket.send(struct.pack('!L', 0))
+        else:
+            # bytearray
+            for n in range(1 + int(len(stream)/chunk_size)):
+                chunk = stream[n*chunk_size:(n+1)*chunk_size]
+                size = struct.pack('!L', len(chunk))
+                try:
+                    self.clamd_socket.send(size)
+                    self.clamd_socket.send(chunk)
+                except socket.error:
+                    raise
+            else:
+                # Terminating stream
+                self.clamd_socket.send(struct.pack('!L', 0))
 
         result='...'
         dr = {}
@@ -522,10 +599,6 @@ class _ClamdGeneric(object):
         return dr
 
 
-
-
-
-
     def _send_command(self, cmd):
         """
         `man clamd` recommends to prefix commands with z, but we will use \n
@@ -543,7 +616,7 @@ class _ClamdGeneric(object):
         receive response from clamd and strip all whitespace characters
         """
         # If we connect too quickly
-        # sometimes we get a connexion error
+        # sometimes we get a connection error
         # so we retry
         failed_count = 5
         while True:
